6442d5e63008cda6d4553982ee88626006f726e3
advaith-22 <popular9adu@gmail.com>
2023-01-06 20:13:53 +0530

    First

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import mediapipe as mp
 import cv2
 
-cap = cv2.VideoCapture(0)
-facemd = mp.solutions.face_mesh
-face = facemd.FaceMesh(refine_landmarks = True)
-facecon = mp.solutions.face_mesh_connections
+class Ft():
+    def __init__(self):
+        self.facemd = mp.solutions.face_mesh
+        self.face = self.facemd.FaceMesh(refine_landmarks = True)
+        self.facecon = mp.solutions.face_mesh_connections
 
-while 1:
-    success, read = cap.read()
-    read = cv2.flip(read, 1)
-    rgb = cv2.cvtColor(read, cv2.COLOR_BGR2RGB)
-    result = face.process(rgb)
-    mesh = result.multi_face_landmarks
-    h, w, _ = read.shape
-    if mesh:
-        landmarks = mesh[0].landmark
-        for landmark in landmarks:
-            x = int(landmark.x * w)
-            y = int(landmark.y * h)
-
-    cv2.imshow("Video", read)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    def show_lm(self, img):
+        img = cv2.flip(img, 1)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = self.face.process(rgb)
+        mesh = result.multi_face_landmarks
+        h, w, _ = img.shape
+        if mesh:
+            landmarks = mesh[0].landmark
+            for landmark in landmarks:
+                x = int(landmark.x * w)
+                y = int(landmark.y * h)
+                cv2.circle(img, (x, y), 5, (0, 0, 255), 10, cv2.FILLED)
+        return img
+    def track(self, img, show_lm=True):
+        img = cv2.flip(img, 1)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = self.face.process(rgb)
+        self.mesh = result.multi_face_landmarks
+        self.h, self.w, _ = img.shape
+        if self.mesh:
+            landmarks = self.mesh[0].landmark
+            for landmark in landmarks:
+                x = int(landmark.x * self.w)
+                y = int(landmark.y * self.h)
+                if show_lm:
+                    cv2.circle(img, (x, y), 2, (0, 0, 255), 2, cv2.FILLED)
+        return img
+    def get_lm(self):
+        lm = []
+        if self.mesh:
+            landmarks = self.mesh[0].landmark
+            for landmark in landmarks:
+                x = int(landmark.x * self.w)
+                y = int(landmark.y * self.h)
+                lm.append([x, y])
+        return lm
